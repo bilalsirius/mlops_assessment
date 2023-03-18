377a659e56cadc6c43e50831098c5503f7a6f6db
bilal12542 <bilal.12542@gmail.com>
2023-03-19 00:21:58 +0500

    chanes

--- app.py
+++ app.py
@@ -10,7 +10,11 @@ from mlops.model import Onnx
 sys.path.remove(parent_dir)
 onnx_model = Onnx()
 
-@app.route('/predict', methods=['POST'])
+print("app file chali")
+
+def runf():
+    print("ye chala?")
+# @app.route('/predict', methods=['POST'])
 def predict():
     print(request.files["image"])
     img_bytes = request.files['image'].read()
@@ -19,4 +23,5 @@ def predict():
     return jsonify(prediction)
 
 if __name__ == '__main__':
+    runf()
     app.run(host='0.0.0.0', port=8080, debug=True)
--- testing.py
+++ testing.py
@@ -2,7 +2,7 @@ import banana_dev as banana
 import json
 
 api_key = "c18de9da-b28e-4681-9ddf-0ed4e929a237"
-model_key = "fc518ead-5511-4b36-9f7d-8face6b1ca60"
+model_key = "2fd9b0b6-d4c3-4dd0-9088-c3fa736cee22"
 model_inputs = json.dumps({"image":"abc"}) # anything you want to send to your model
 
 out = banana.run(api_key, model_key, model_inputs)
