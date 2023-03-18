9bd7a98b894801749911cfbaee9354b35df72757
bilal12542 <bilal.12542@gmail.com>
2023-03-19 02:16:25 +0500

    changes

--- testing.py
+++ testing.py
@@ -1,9 +1,9 @@
-import banana_dev as banana
-import json
+# import banana_dev as banana
+# import json
 
-api_key = "c18de9da-b28e-4681-9ddf-0ed4e929a237"
-model_key = "2fd9b0b6-d4c3-4dd0-9088-c3fa736cee22"
-model_inputs = json.dumps({"image":"abc"}) # anything you want to send to your model
+# api_key = "c18de9da-b28e-4681-9ddf-0ed4e929a237"
+# model_key = "2fd9b0b6-d4c3-4dd0-9088-c3fa736cee22"
+# model_inputs = json.dumps({"image":"abc"}) # anything you want to send to your model
 
-out = banana.run(api_key, model_key, model_inputs)
-print(out)
+# out = banana.run(api_key, model_key, model_inputs)
+# print(out)
