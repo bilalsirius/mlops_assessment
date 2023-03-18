55f444f8ee12bd3ace16596ca259d3597b6d829d
bilal12542 <bilal.12542@gmail.com>
2023-03-19 02:28:37 +0500

    changes

--- mlops/convert_to_onnx.py
+++ mlops/convert_to_onnx.py
@@ -1,33 +1,33 @@
-from PIL import Image
-import onnx
-import torch
-import sys
-import os
-import traceback
+# from PIL import Image
+# import onnx
+# import torch
+# import sys
+# import os
+# import traceback
 
-parent_dir = os.path.dirname(__file__)
-sys.path.append(parent_dir)
-from pytorch_model import Classifier, BasicBlock
-sys.path.remove(parent_dir)
+# parent_dir = os.path.dirname(__file__)
+# sys.path.append(parent_dir)
+# from pytorch_model import Classifier, BasicBlock
+# sys.path.remove(parent_dir)
 
-if __name__ == "__main__":
-	try:
-		# print("Path:", sys.argv[0])
-		# if sys.argv[0]:
-		mtailor = Classifier(BasicBlock, [2, 2, 2, 2])
-		trained_model_path = os.path.join(".", "weights", "pytorch_model_weights_1.pth")
-		mtailor.load_state_dict(torch.load(trained_model_path))
-		mtailor.eval()
-		print("model loaded!")
-		img = Image.open(os.path.join(".", "..", "test_cases", "images", "n01667114_mud_turtle.jpeg"))
-		dummy_data = mtailor.preprocess_numpy(img).unsqueeze(0)
-		onnx_model_name = os.path.join(parent_dir, trained_model_path.split('/')[-1].split(".")[0] + ".onnx")
-		torch.onnx.export(mtailor, dummy_data, onnx_model_name, verbose=True)
-		onnx_model = onnx.load(onnx_model_name)
-		print("Model Converted!!!!")
-		#onnx.checker.check_model(onnx_model)
-		# else:
-		# 	print("no command line argument found!")
-	except:
-		print(traceback.print_exc())
+# if __name__ == "__main__":
+# 	try:
+# 		# print("Path:", sys.argv[0])
+# 		# if sys.argv[0]:
+# 		mtailor = Classifier(BasicBlock, [2, 2, 2, 2])
+# 		trained_model_path = os.path.join(".", "weights", "pytorch_model_weights_1.pth")
+# 		mtailor.load_state_dict(torch.load(trained_model_path))
+# 		mtailor.eval()
+# 		print("model loaded!")
+# 		img = Image.open(os.path.join(".", "..", "test_cases", "images", "n01667114_mud_turtle.jpeg"))
+# 		dummy_data = mtailor.preprocess_numpy(img).unsqueeze(0)
+# 		onnx_model_name = os.path.join(parent_dir, trained_model_path.split('/')[-1].split(".")[0] + ".onnx")
+# 		torch.onnx.export(mtailor, dummy_data, onnx_model_name, verbose=True)
+# 		onnx_model = onnx.load(onnx_model_name)
+# 		print("Model Converted!!!!")
+# 		#onnx.checker.check_model(onnx_model)
+# 		# else:
+# 		# 	print("no command line argument found!")
+# 	except:
+# 		print(traceback.print_exc())
 		
--- testing.py
+++ testing.py
@@ -1,9 +1,9 @@
 # import banana_dev as banana
 # import json
 
-# api_key = "c18de9da-b28e-4681-9ddf-0ed4e929a237"
-# model_key = "2fd9b0b6-d4c3-4dd0-9088-c3fa736cee22"
-# model_inputs = json.dumps({"image":"abc"}) # anything you want to send to your model
+# api_key = "610bf20c-edd9-46dd-b451-837860afbcb5"
+# model_key = "3ba37889-e950-4650-954f-1526431a7832"
+# model_inputs = {"image":"abc"} # anything you want to send to your model
 
 # out = banana.run(api_key, model_key, model_inputs)
 # print(out)
