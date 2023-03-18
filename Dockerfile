ca32abcde3a8d56ba7a43a56faaff6d3dfb6a5b9
bilal12542 <bilal.12542@gmail.com>
2023-03-19 02:48:26 +0500

    changes

--- Dockerfile
+++ Dockerfile
@@ -8,7 +8,6 @@ WORKDIR /app
 RUN python -m pip install --upgrade pip
 RUN pip install -r requirements.txt
 
-EXPOSE 8080
 
 # Run the command to start the application
-CMD ["python","app.py"]
+CMD python server.py
---
+++ server.py
@@ -0,0 +1,49 @@
+# Do not edit if deploying to Banana Serverless
+# This file is boilerplate for the http server, and follows a strict interface.
+
+# Instead, edit the init() and inference() functions in app.py
+
+from flask import Flask, request as response, jsonify
+import subprocess
+import app as user_src
+
+# We do the model load-to-GPU step on server startup
+# so the model object is available globally for reuse
+user_src.init()
+
+# Create the http server app
+server = Flask(__name__)
+
+# Healthchecks verify that the environment is correct on Banana Serverless
+@server.route('/healthcheck', methods=["GET"])
+def healthcheck(request):
+    # dependency free way to check if GPU is visible
+    gpu = False
+    out = subprocess.run("nvidia-smi", shell=True)
+    if out.returncode == 0: # success state on shell command
+        gpu = True
+
+    return response.json({"state": "healthy", "gpu": gpu})
+
+# Inference POST handler at '/' is called for every http call from Banana
+@server.route('/', methods=["POST"]) 
+def inference(request):
+    try:
+        model_inputs = response.json.loads(request.json)
+    except:
+        model_inputs = request.json
+
+    output = user_src.inference(model_inputs)
+
+    return response.json(output)
+
+@server.route('/predict', methods=['POST'])
+def predict(request):
+    print(request)
+    # img_data = re.files['image'].read()
+    # Preprocess the image data if necessary
+    # prediction = onnx_model.predict(img_data)
+    # return jsonify(prediction)
+
+if __name__ == '__main__':
+    server.run(host='0.0.0.0', port=8000, workers=1)
