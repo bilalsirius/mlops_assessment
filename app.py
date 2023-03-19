afd7c3ade83cd708d3b27b9dc6e8289065ef968f
bilal12542 <bilal.12542@gmail.com>
2023-03-19 14:12:44 +0500

    fix app.py

--- app.py
+++ app.py
@@ -4,17 +4,21 @@ import json
 from urllib.parse import urlparse
 import os
 import requests
+import sys
+
+parent_dir = os.path.dirname(__file__)
+sys.path.append(parent_dir)
+from mlops.model import Onnx
+sys.path.remove(parent_dir)
 
 def init():
     global model
-    from mlops.model import Onnx
     model = Onnx()
     print("model loaded!!")
     os.system("python -m unittest discover test_cases/")
 
 def inference(model_inputs:dict) -> dict:
     global model
-    return model_inputs
     try:
         model_inputs = json.loads(model_inputs)
     except:
@@ -22,11 +26,11 @@ def inference(model_inputs:dict) -> dict:
     try:
         if "image" in model_inputs:
             img_p = model_inputs["image"]
-            if urlparse(img_p).scheme:
-                img = requests.get(img_p) 
-                img = base64.b64decode(img)
-            else:
-                img = open(f".{img_p}", "rb").read()
+            # if urlparse(img_p).scheme:
+            #     img = requests.get(img_p) 
+            #     img = base64.b64decode(img)
+            # else:
+            img = open(f".{img_p}", "rb").read()
             img = model.preprocess_cv2(img)
             pred = model.prediction(img)
             return pred
